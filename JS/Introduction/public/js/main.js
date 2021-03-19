17447e74037be9e9a7ecb544ef8bcdc2f86368ad
Roxuno <80962586+Roxuno@users.noreply.github.com>
2021-03-19 16:18:32 +0100

    CodingWeek

---
+++ JS/Introduction/public/js/main.js
@@ -0,0 +1,9 @@
+var monPrenom = 'Mohammed'
+console.log(monPrenom)
+var monNom = 'Bahoum'
+console.log(monNom)
+var monAge = '23'
+console.log(monAge)
+console.log(typeof(monAge))
+
+alert("Je m'appelle" + ' ' + monPrenom + ' ' + monNom + ' ' + "et j'ai" + ' ' + monAge + ' ' + "ans")
---
+++ JS/Math/public/js/main.js
@@ -0,0 +1,49 @@
+console.log(25 + 5);
+console.log(25 - 5);
+console.log(25 * 5);
+console.log(25 / 5);
+
+console.log('________________________');
+
+var a = 25
+var b = 5
+
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log('________________________');
+
+var c = a + b
+var d = a - b
+var e = a * b
+var f = a / b
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+console.log('________________________');
+
+console.log("Le calcul" + " " + a + "+" + b + "=" + c)
+console.log("Le calcul" + " " + a + "-" + b + "=" + d)
+console.log("Le calcul" + " " + a + "*" + b + "=" + e)
+console.log("Le calcul" + " " + a + "/" + b + "=" + f)
+
+console.log('________________________');
+
+var verre1 = 'eau'
+var verre2 = 'jus'
+var verre3 = 'coca'
+
+var verreVide = verre1
+var verre1 = verre3
+var verre3 = verre2
+var verre2 = verreVide
+
+console.log(verre1)
+console.log(verre2)
+console.log(verre3)
